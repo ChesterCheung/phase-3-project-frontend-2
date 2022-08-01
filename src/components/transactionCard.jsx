2b16734e371440c1323370d0d002bf0a58df3a7b
Chester Cheung <cheung319@gmail.com>
2022-08-01 16:42:16 -0400

    created components for transactions

---
+++ src/components/transactionCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const transactionCard = () => {
+  return (
+    <div>transactionCard</div>
+  )
+}
+
+export default transactionCard
---
+++ src/components/transactionList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const transactionList = () => {
+  return (
+    <div>transactionList</div>
+  )
+}
+
+export default transactionList
