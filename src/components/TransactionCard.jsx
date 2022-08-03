786d0f49233a4a841ca45ca331f002fb8e80d2c9
Chester Cheung <cheung319@gmail.com>
2022-08-03 15:27:05 -0400

    Updated transaction card

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const TransactionCard = ( {transaction} ) => {
+    const [isEditing, setIsEditing] = useState(false)
 
   return (
     <div>
-      <h3>{transaction.company_name}</h3>
-      {/* {console.log(transaction.investor)} */}
+      <li style={{ listStyleType: "none" }}>
+        <p>
+            Stock:{transaction.company_name} - Price: {transaction.price} - Date: {transaction.date}
+        </p>
+      </li>
     </div>
   )
 }
 
 export default TransactionCard
+
+// { blog.author ? <p><em>By: <Link to={`/authors/${blog.author.id}`}>{ blog.author.name}</Link></em></p> : <p><em>By: <Link to={`/authors/${author.id}`}>{ author.name}</Link></em></p>}
