b0a56cbf56b1832c398483b509fe363ca31d5848
Chester Cheung <cheung319@gmail.com>
2022-08-03 17:29:15 -0400

    added links for investor transactions

--- src/components/Investors.jsx
+++ src/components/Investors.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import TransactionCard from './TransactionCard'
 
 const Investors = () => {
+    const [investors, setInvestors] = useState({stock_transactions:[]})
+    const {id} = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/investors/${id}`)
+            .then(resp => resp.json())
+            .then(data => setInvestors(data))
+    }, [])
+
+    const transactionCards = investors.stock_transactions.map(transaction => {
+        return <TransactionCard transaction={ transaction } key={ transaction.id } investor={ investors }/>
+    })
+
   return (
-    <div>investors</div>
+    <div>
+      <h1>{ investors.name }</h1>
+      { transactionCards }
+    </div>
   )
 }
 
